9ab3177f5c1657c7a573e7c7648d1b2cd24714ae
ojporv <olli.porvari@helsinki.fi>
2021-08-29 22:56:48 +0300

    2.4 ja 2.5 tehty

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -1,29 +1,31 @@
 import React from 'react'
 
-const Header = ({ course }) => <h1>{course.name}</h1>
+//Prints out the course header
+const Header = ({ course }) => <h2>{course.name}</h2>
 
+//Prints out a course content's name and amount of exercises in it
 const Part = ({ name, exercise }) => {
     console.log('Part')
   return (
-    <li>
+    <p>
       {name} {exercise}
-    </li>
+    </p>
   )
 }
 
+//Maps out course contents name and exercise amount for Part component
 const Content = ({ parts }) => {
   console.log('Content', parts)
   return (
   <div>
-    <ul>
-        {parts.map(part =>
-            <Part key={part.name} name={part.name} exercise={part.exercises} />
-        )}
-    </ul>
+    {parts.map(part =>
+        <Part key={part.id} name={part.name} exercise={part.exercises} />
+    )}
   </div>
   )
 }
 
+//Counts total amount of exercises and prints it out
 const Total = ({ parts }) => {
     const totalAmount = parts
       .map(part => part.exercises)
@@ -32,16 +34,23 @@ const Total = ({ parts }) => {
         return total + count}, 0)
         
     return (
-        <p>Total of {totalAmount} exercises </p>
+        <h4>Total of {totalAmount} exercises </h4>
     )
 }
 
-const Course = ({ course }) => {
-    return (
+//Maps out courses array as individual courses for header, content and total components
+const Course = ({ courses }) => {
+  return (
         <div>
-            <Header course={course} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
+          <h1>Web Development Curriculum</h1>
+          {courses.map(course => (
+            <div key={course.id}>
+              <Header course={course} />
+              <Content parts={course.parts} />
+              <Total parts={course.parts} />
+            </div>
+            )
+          )}
         </div>
     )
 }
