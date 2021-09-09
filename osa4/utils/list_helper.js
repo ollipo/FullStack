6e83015baaf39427ad4382984ccd6504e7ca6d20
ojporv <olli.porvari@helsinki.fi>
2021-09-09 13:40:42 +0300

    4.5 tehty

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -6,14 +6,21 @@ const totalLikes = (blogs) => {
     const reducer = (sum, item) => {
         return sum + item
       }
-    return blogs.length === 0
-        ? 0 
-        : blogs
+    return blogs
             .map(blog => blog.likes)
             .reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    const listOfFavorites = blogs.map(blog => blog.likes)
+    const biggestLikesCount = Math.max(...listOfFavorites)
+    const mostFavoriteBlog = blogs.find(blog => blog.likes === biggestLikesCount)
+
+    return mostFavoriteBlog
 }
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
