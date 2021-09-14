d9e2ff5bbdb915d89c3d14cef0831a3a56925383
ojporv <olli.porvari@helsinki.fi>
2021-09-14 22:09:46 +0300

    4.20 tehty

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -11,21 +11,12 @@ blogsRouter.get('/', async (request, response) => {
 	response.json(blogs.map(blog => blog.toJSON()))
 })
 
-const getTokenFrom = request => {
-	const authorization = request.get('authorization')
-	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-		return authorization.substring(7)
-	}
-	return null
-}
-
 blogsRouter.post('/', async (request, response) => {
 	const body = request.body
 
-	const token = getTokenFrom(request)
 	// eslint-disable-next-line no-undef
-	const decodedToken = jwt.verify(token, process.env.SECRET)
-	if (!token || !decodedToken.id) {
+	const decodedToken = jwt.verify(request.token, process.env.SECRET)
+	if (!request.token || !decodedToken.id) {
 		return response.status(401).json({ error: 'token missing or invalid' })
 	}
 	const user = await User.findById(decodedToken.id)
