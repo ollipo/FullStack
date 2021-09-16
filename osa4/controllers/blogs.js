425b2f2214bc59f69709f963795963588e08ff0f
ojporv <olli.porvari@helsinki.fi>
2021-09-16 22:37:45 +0300

    5.1 yhteydessä tehty korjaus userExtractoriin

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -2,16 +2,18 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+const middleware = require('../utils/middleware')
 
 
 blogsRouter.get('/', async (request, response) => {
-	const blogs = await Blog
-		.find({}).populate('user', { username: 1, name: 1 })
-
-	response.json(blogs.map(blog => blog.toJSON()))
+	const blogs =
+		await Blog
+			.find({})
+			.populate('user', { username: 1, name: 1 })
+	response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
 	const body = request.body
 
 	// eslint-disable-next-line no-undef
@@ -21,9 +23,7 @@ blogsRouter.post('/', async (request, response) => {
 		return response.status(401).json({ error: 'token missing or invalid' })
 	}
 	const user = request.user
-
 	const userDb = await User.findById(user.id)
-
 	const blog = new Blog({
 		title: body.title,
 		author: body.author,
@@ -39,7 +39,7 @@ blogsRouter.post('/', async (request, response) => {
 	response.status(201).json(savedBlog.toJSON())
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
 	// eslint-disable-next-line no-undef
 	const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
@@ -60,11 +60,9 @@ blogsRouter.delete('/:id', async (request, response) => {
 
 blogsRouter.put('/:id', async (request, response) => {
 	const body = request.body
-
 	const blog = {
 		likes: body.likes,
 	}
-
 	const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
 	response.json(updatedBlog)
 })
