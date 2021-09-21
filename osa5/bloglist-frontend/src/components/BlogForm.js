dfae2d5bcb46cb7941ca80f4556b6bdd31cf5dbe
ojporv <olli.porvari@helsinki.fi>
2021-09-21 07:33:09 +0300

    5.11 tehty

--- osa5/bloglist-frontend/src/components/BlogForm.js
+++ osa5/bloglist-frontend/src/components/BlogForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import PropTypes from 'prop-types'
 
 const BlogForm = ({ createBlog }) => {
     const [title, setTitle] = useState('')
@@ -49,6 +50,10 @@ const BlogForm = ({ createBlog }) => {
       <button type="submit">create</button>
     </form>  
   )
+}
+
+BlogForm.propTypes = {
+  createBlog: PropTypes.func.isRequired
 }
 
 export default BlogForm
