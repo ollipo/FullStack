9e75f3b4c45c5815d44385084bd69fee30889b40
ojporv <olli.porvari@helsinki.fi>
2021-09-23 10:19:59 +0300

    5.18 tehty

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,10 +1,35 @@
 describe('Blog app', function() {
 	beforeEach(function() {
 		cy.request('POST', 'http://localhost:3003/api/testing/reset')
+		const user = {
+			name: 'Olli',
+			username: 'ollipo7',
+			password: 'top secret7'
+		}
+		cy.request('POST', 'http://localhost:3003/api/users/', user)
 		cy.visit('http://localhost:3000')
 	})
 
 	it('Login form is shown', function() {
 		cy.contains('login')
 	})
+
+	describe('Login',function() {
+		it('succeeds with correct credentials', function() {
+			cy.get('#username').type('ollipo7')
+			cy.get('#password').type('top secret7')
+			cy.get('#login-button').click()
+			cy.contains('Olli logged in')
+		})
+
+		it('fails with wrong credentials', function() {
+			cy.get('#username').type('ollipo7')
+			cy.get('#password').type('wrong')
+			cy.get('#login-button').click()
+
+			cy.get('.errorMessage').contains('Wrong or missing username or password')
+			cy.get('.errorMessage').should('have.css', 'color', 'rgb(255, 0, 0)')
+		})
+	})
+
 })
