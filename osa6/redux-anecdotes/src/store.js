7d613cd2c104e9facfd8cea79c2adb56b23788cc
ojporv <olli.porvari@helsinki.fi>
2021-09-27 15:23:02 +0300

    6.15 tehty

--- osa6/redux-anecdotes/src/store.js
+++ osa6/redux-anecdotes/src/store.js
@@ -1,4 +1,6 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
@@ -8,6 +10,11 @@ const reducer = combineReducers({
   notification: notificationReducer,
   filter: filterReducer
 })
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+)
 
 export default store
