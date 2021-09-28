54268eaa40ad8b6554bb9e283c10fca634d9eb7b
ojporv <olli.porvari@helsinki.fi>
2021-09-28 11:02:29 +0300

    6.19 tehty

--- osa6/redux-anecdotes/src/components/Notification.js
+++ osa6/redux-anecdotes/src/components/Notification.js
@@ -1,9 +1,8 @@
-
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
+  const notification = props.notification
   if (notification === null) {
     return null
   }
@@ -19,4 +18,11 @@ const Notification = () => {
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
