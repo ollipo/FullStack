053013f3a05906d6857b719079e347cbe6d60d65
ojporv <olli.porvari@helsinki.fi>
2021-09-28 11:15:08 +0300

    6.20 tehty

--- osa6/redux-anecdotes/src/components/Filter.js
+++ osa6/redux-anecdotes/src/components/Filter.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { setFilter } from '../reducers/filterReducer'
 
-const Filter = () => {
-    const dispatch = useDispatch()
+const Filter = (props) => {
 
     const handleChange = (event) => {
         const filter = event.target.value
-        dispatch(setFilter(filter))
+        props.setFilter(filter)
     }
     const style = {
         marginBottom: 10
@@ -20,4 +19,7 @@ const Filter = () => {
     )
 }
 
-export default Filter
+export default connect(
+    null,
+    { setFilter }
+)(Filter)
