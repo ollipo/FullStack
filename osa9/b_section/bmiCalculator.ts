76b8f7096000264651539ebf9e76f2aa863e70a1
ojporv <olli.porvari@helsinki.fi>
2021-10-29 22:21:04 +0300

    9.3 tehty

--- osa9/b_section/bmiCalculator.ts
+++ osa9/b_section/bmiCalculator.ts
@@ -1,5 +1,24 @@
-const calculateBmi = (a: number, b: number) : String => {
-    const bmi = b / ((a/100) * (a/100))
+interface MultiplyValues {
+    value1: number;
+    value2: number;
+  }
+  
+  const parseArguments = (args: Array<string>): MultiplyValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+  
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+      return {
+        value1: Number(args[2]),
+        value2: Number(args[3])
+      }
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+  }
+  
+  const calculateBmi = (height: number, weight: number) : String => {
+    const bmi = weight / ((height/100) * (height/100))
 
     if(bmi < 18.5) {
         return 'Underweight';
@@ -12,5 +31,14 @@ const calculateBmi = (a: number, b: number) : String => {
     }
         
 }
-
-console.log(calculateBmi(180, 74))
+  
+  try {
+    const { value1, value2 } = parseArguments(process.argv);
+    console.log(calculateBmi(value1, value2));
+  } catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if(error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
--- osa9/b_section/exerciseCalculator.ts
+++ osa9/b_section/exerciseCalculator.ts
@@ -1,49 +1,69 @@
 interface Result {
-    periodLength: number;
-    trainingDays: number;
-    success: false
-    rating: number;
-    ratingDescription: String;
-    target: number;
-    average: number;
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: String;
+  target: number;
+  average: number;
+}
+
+const exerciseCalculator = (targetAmount: number, exerciseHours: Array<number>) : Result => {
+
+  const trainingDays = exerciseHours.filter(d => d !== 0).length
+  const average = exerciseHours.reduce((a, b) => a + b) / exerciseHours.length
+  let rating = 0
+  let ratingDescription = ''
+      if(average < 0.5*targetAmount) {
+          rating = 1
+          ratingDescription = 'You could do better!'
+      } else if (average < targetAmount) {
+          rating = 2
+          ratingDescription = 'Good effort! A bit more next time!'
+      } else if (average >= targetAmount) {
+          rating = 3
+          ratingDescription = 'You reached you\'re goal! Nicely done!'
+      }
+
+  console.log({
+      periodLength: exerciseHours.length,
+      trainingDays: trainingDays,
+      success: average >= targetAmount ? true : false,
+      rating: rating,
+      ratingDescription: ratingDescription,
+      target: targetAmount,
+      average: average
+  })
+
+  return {
+      periodLength: exerciseHours.length,
+      trainingDays: trainingDays,
+      success: false,
+      rating: rating,
+      ratingDescription: ratingDescription,
+      target: targetAmount,
+      average: average
   }
+}
 
-  const exerciseCalculator = (exerciseHours: Array<number>, targetAmount: number) : Result => {
-
-    const trainingDays = exerciseHours.filter(d => d !== 0).length
-    const average = exerciseHours.reduce((a, b) => a + b) / exerciseHours.length
-    let rating = 0
-    let ratingDescription = ''
-        if(average < 0.5*targetAmount) {
-            rating = 1
-            ratingDescription = 'You could do better!'
-        } else if (average < targetAmount) {
-            rating = 2
-            ratingDescription = 'Could effort! A bit more next time!'
-        } else if (average >= targetAmount) {
-            rating = 3
-            ratingDescription = 'You reached you\'re goal! Nicely done!'
-        }
-
-    console.log({
-        periodLength: exerciseHours.length,
-        trainingDays: trainingDays,
-        success: average >= targetAmount ? true : false,
-        rating: rating,
-        ratingDescription: ratingDescription,
-        target: targetAmount,
-        average: average
-    })
-
-    return {
-        periodLength: exerciseHours.length,
-        trainingDays: trainingDays,
-        success: false,
-        rating: rating,
-        ratingDescription: ratingDescription,
-        target: targetAmount,
-        average: average
+  const parseCalcArguments = (args: Array<string>): Array<number> => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    const argsInput =args.slice(2);
+    if(argsInput.every(a => !isNaN(Number(a)))) {
+      const result = argsInput.map(a => Number(a));
+      return result
+    } else {
+      throw new Error('Provided values were not numbers!');
     }
   }
-
-  exerciseCalculator([3, 0, 2, 4.5, 0, 3, 1], 2)
+    
+  try {
+    let args = parseCalcArguments(process.argv);
+    exerciseCalculator(args[0], args.slice(1));
+  } catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if(error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+  }
